60debce95a1afd967bd80c9e800459708cad268e
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-07-14 22:43:47 +0800

    update

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,6 +1,25 @@
 #include <gtest/gtest.h>
 
+
+std::string fizzBuzz(int value)
+{
+    if()
+        return "Fizz";
+    if()
+        return "Buzz";
+    return std::to_string(value);
+}
+
+
+void checkFizzBuzz(int value, std::string expectedResult)
+{
+    std::string result = fizzBuzz(value);
+    ASSERT_STREQ(expectedResult.c_str(), result.c_str());
+}
+
 TEST(FizzBuzzTest, doesPass)
 {
-        ASSERT_TRUE(true);
+    ASSERT_TRUE(true);
 }
+
+TEST(FizzBuzzTest)
